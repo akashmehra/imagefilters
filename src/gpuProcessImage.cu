caed56cfde52765fcb91b68f18342c5f7c1b5f09
Akash Mehra <mehra.akash@gmail.com>
2012-04-21 21:53:33 -0400

    added a wrappe for calling the gpu kernel function.

--- src/gpuProcessImage.cu
+++ src/gpuProcessImage.cu
@@ -5,31 +5,32 @@
 #include <stdio.h>
 #include "CImg.h"
 
-#include "processImage.h"
+#include "cpuProcessImage.h"
 #include "Filters.h"
 #include "Utils.h"
 #include "Constants.h"
 using namespace cimg_library;
 
 template<typename T>
-__device__ T DomainCheck(T pixel)
+__device__ T brightness(const T& pixel, float bValue)
 {
-  PIXEL_DOMAIN_CHECK(pixel);
-  return pixel;
+	int val = (int)(pixel*bValue);
+  PIXEL_DOMAIN_CHECK(val);
+  return val;
 }
 
 template <typename T>
-__global__ void processingKernel(T* inputBuffer, T* outputBuffer, int width, 
-                            int height, int channels,const int offset)
+__global__ void imageKernel(T* inputBuffer, T* outputBuffer, int width, 
+                            int height, int channels,int offset)
 {
   
   int redChannelOffset = blockIdx.x * blockDim.x + threadIdx.x;
   int greenChannelOffset = redChannelOffset + 1*offset;
   int blueChannelOffset = redChannelOffset + 2*offset;
   float brightnessVal = 0.0f;
-  outputBuffer[redChannelOffset] = (T)DomainCheck<float>(50+inputBuffer[redChannelOffset]);
-  outputBuffer[greenChannelOffset] = (T)DomainCheck<float>(50+inputBuffer[greenChannelOffset]);
-  outputBuffer[blueChannelOffset] = (T)DomainCheck<float>(50+inputBuffer[blueChannelOffset]);
+  outputBuffer[redChannelOffset] =   brightness(inputBuffer[redChannelOffset],1.2);
+  outputBuffer[greenChannelOffset] = brightness(inputBuffer[greenChannelOffset],1.2);
+  outputBuffer[blueChannelOffset] =  brightness(inputBuffer[blueChannelOffset],1.2);
   __syncthreads();
 }
 
@@ -47,6 +48,15 @@ unsigned int powerOf2( unsigned int x ) {
   x |= x >> 8;
   x |= x >> 16;
   return ++x;
+}
+
+template <typename T>
+void callKernel(void(*kernel)(T*,T*,int,int,int), int blocks, int threads, 
+          T* inputBuffer,T* outputBuffer,int wdth, int height, int channels, int offset) 
+{
+    dim3 dimGrid(blocks,1,1);
+    dim3 dimBlock(threads,1,1);
+    (*kernel)<T><<<dimGrid,dimBlock>>>(inputBuffer, outputBuffer, width, height, channels, offset);
 }
 
 
@@ -76,8 +86,8 @@ void runKernel(unsigned char* h_data, unsigned char* h_result,const unsigned int
   
   Setup setup;
   getSetupConfig(width*height,&setup);
-  std::cout << "Blocks: " << setup.blocks << std::endl;
-  std::cout << "Threads: " << setup.threads << std::endl;
+  //std::cout << "Blocks: " << setup.blocks << std::endl;
+  //std::cout << "Threads: " << setup.threads << std::endl;
 
   unsigned char *d_data;
   cudaMalloc((void**)&d_data,sizeData);
@@ -86,11 +96,13 @@ void runKernel(unsigned char* h_data, unsigned char* h_result,const unsigned int
   unsigned char* d_result;
   cudaMalloc((void**)&d_result,sizeData);
   
-  dim3 dimGrid(setup.blocks,1,1);
-  dim3 dimBlock(setup.threads,1,1);
+  //dim3 dimGrid(setup.blocks,1,1);
+  //dim3 dimBlock(setup.threads,1,1);
   int sizeSharedMem = problemSize;
   int offset = width*height;
-  processingKernel<unsigned char><<<dimGrid,dimBlock>>>(d_data,d_result,width,height,channels, offset);
+  //processingKernel<unsigned char><<<dimGrid,dimBlock>>>(d_data,d_result,width,height,channels, offset);
+  callKernel<unsigned char>(imageKernel,setup.blocks,setup.threads,
+                            d_data,d_result,width,height,channels, offset);
   
   cudaMemcpy(h_result,d_result,sizeResult,
              cudaMemcpyDeviceToHost);
@@ -141,7 +153,7 @@ int main(int argc, char* argv[])
     
     sendWarmUpSignal(inputBuffer,imgInfo.width*imgInfo.height*imgInfo.spectrum); 
     double dTime2 = gpu::getTime(tim);
-    std::cout << "time taken for unrolled version: " << dTime2 - dTime1 << std::endl;
+    std::cout << "time taken for performing warm up: " << dTime2 - dTime1 << std::endl;
     runKernel(inputBuffer,outputBuffer,imgInfo.width, imgInfo.height, imgInfo.spectrum);
     
     //imp.saturation(S_VALUE,inputBuffer, outputBuffer, 
@@ -151,7 +163,7 @@ int main(int argc, char* argv[])
                                     imgInfo.spectrum,0);
     
     double dTime3 = gpu::getTime(tim);
-    std::cout << "time taken for unrolled version: " << dTime3 - dTime2 << std::endl;
+    std::cout << "time taken for GPU operation: " << dTime3 - dTime2 << std::endl;
     
     outputImage.save_jpeg(outputFilename.c_str());
     CImgDisplay darkDisplay(outputImage,"Output Image",0);

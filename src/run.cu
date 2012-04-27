e07daddebad330f8c383359623656584609f0fd5
Akash Mehra <mehra.akash@gmail.com>
2012-04-27 08:47:53 -0400

    GPU profiler code.

--- src/run.cu
+++ src/run.cu
@@ -1,47 +1,79 @@
 #include <iostream>
-#include "CImg.h"
-#include "gpuProcessImage.cu"
-#include "Utils.h"
-#include "cpuProcessImage.h"
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <vector>
 #include <string>
+#include <cstdlib>
+#include "CImg.h"
+
+#include "gpuProcessImage.cu"
+#include "Utils.h"
+#include "cpuProcessImage.h"
+
 
 int main(int argc, char* argv[])
 {
-  //cimg::imagemagick_path("/opt/local/bin/convert");
-	  if(argc == 2)                                                                                                              
-    {
-  		std::vector<std::string> fileList;
-  		std::string directoryPath = argv[1];
+
+		gpu::Options options;
+	 	bool validArguments	= parseCommandLine(argc, argv, &options);	 
+  	if(validArguments)
+		{
+			std::vector<std::string> fileList;
+  		std::string directoryPath = options.directoryPath;
+
   		gpu::readDirectory(directoryPath,&fileList);
   		std::vector<std::string>::iterator it = fileList.begin();
      	int count = 0;
-      //int convKernel[] = {-2,-1,0,-1,1,1,0,1,2};	
-  		for(;it != fileList.end();++it)
+  		
+			int warmpupBuffer = calloc(1000, sizeof(float));
+
+      timeval tim;
+      double dTime1 = gpu::getTime(tim);
+					
+			std::cout << "Sending warm up signal to GPU." << std::endl;
+   	 	
+			sendWarmUpSignal(warmupBuffer,1000*sizeof(float)); 
+
+			double dTime2 = gpu::getTime(tim);
+			double warmpupTime = dTime2 - dTime1;
+    	std::cout << "time taken for performing warm up: " << warmupTime << std::endl;
+			
+   	 	delete[] warmpupBuffer;
+			
+			std::cout << "Starting File I/O using CPU." << std::endl;
+			double fileIOTime = 0.0;
+
+			double configurationTime = 0.0;
+
+			double executionTime = 0.0;
+
+			for(;it != fileList.end();++it)
   		{
   			std::string imageFilename = directoryPath+*it;
   			std::string extension = imageFilename.substr(imageFilename.length()-4,4); 
   			if(extension == JPG_EXTENSION || extension == JPEG_EXTENSION)
   			{
-  				if(count % 15 == 0)
-  				{
-  					std::cout << "Frame " << count << std::endl;
-  				}
-  				++count;
   				std::string filename = imageFilename.substr(0,imageFilename.length()-4);	
      			int indexOfSlash = imageFilename.find_last_of("/");
-  				std::string outputFilename = imageFilename.substr(indexOfSlash,
+  				
+					std::cout << "Reading Image from Disk." << std::endl;
+					dTime1 = gpu::getTime(tim);
+	
+					std::string outputFilename = imageFilename.substr(indexOfSlash,
 																														imageFilename.length()-4-indexOfSlash);
-      		CImg<unsigned char> image(imageFilename.c_str());
+      		dTime2 = gpu::getTime(tim);
+					fileIOTime += dTime2 - dTime1;
+
+					std::cout << "Time taken to read from disk:  " << dTime2 - dTime1 << std::endl;
+
+					
+					dTime1 = gpu::getTime(tim);
+					CImg<unsigned char> image(imageFilename.c_str());
   
-    		  //CImgDisplay mainDisplay(image,"Image",0);
-      		
+					std::cout << "Unrolling Image and setting up blocks and threads." << std::endl;
 					gpu::Image imgInfo(image.width(),image.height(),image.width()*image.height(),image.spectrum());
-      		//gpu::printMetaData(imgInfo);
-      
-      		/*
+      		
+					/*
        			<summary> 
        			1. Allocate Buffers
        			2. Get Meta information from the image and assign that to ImageInfo object.
@@ -52,30 +84,109 @@ int main(int argc, char* argv[])
       		unsigned char* inputBuffer = new unsigned char[imgInfo.spectrum*imgInfo.size];
       		unsigned char* outputBuffer = new unsigned char[imgInfo.spectrum*imgInfo.size];
       
-      		timeval tim;
-      		double dTime1 = gpu::getTime(tim);
-      
       		gpu::unrollMatrix(image,imgInfo.width,imgInfo.height,imgInfo.spectrum,
                   inputBuffer);
-    
-   	 			sendWarmUpSignal(inputBuffer,imgInfo.width*imgInfo.height*imgInfo.spectrum); 
-   	 			double dTime2 = gpu::getTime(tim);
-    			std::cout << "time taken for performing warm up: " << dTime2 - dTime1 << std::endl;
     			
-					process(inputBuffer,outputBuffer,imgInfo.width, imgInfo.height, imgInfo.spectrum);
-    
+    			
+
+					int problemSize, sizeData, sizeResult;
+          gpu::Setup setup;
+          startSetup(width, height, channels,&problemSize, &sizeData, &sizeResult, &setup);
+          dTime2 = gpu::getTime(tim);
+					
+					configurationTime += dTime2 - dTime1;
+			
+          std::cout << "Blocks: " << setup.blocks << ", Threads: " << setup.threads << std::endl;
+					std::cout << "Done configuring the problem.\nTime taken: " << dTime2 - dTime1 << std::endl;
+
+
+					dTime1 = gpu::getTime(tim);
+					std::cout << "Starting memory allocation and data transfer from Host to Device." << std::endl;
+          unsigned char *d_data;
+          cudaMalloc((void**)&d_data,sizeData);
+          cudaMemcpy(d_data,h_data,sizeData,cudaMemcpyHostToDevice);
+          
+          unsigned char* d_result;
+          cudaMalloc((void**)&d_result,sizeData);
+          
+					dTime2 = gpu::getTime(tim);
+					std::cout << "Done transferring data.\nTime taken: " << dTime2 - dTime1 <<std::endl;
+					
+					dTime1 = gpu::getTime(tim);
+					std::cout << "Begining execution on GPU." << std::endl;
+          int offset = width*height;
+          switch(filterFlag)
+					{									
+
+						/*
+						case BRIGHTNESS:
+          		runBrightnessKernel(setup,d_data,d_result,h_result,
+															width,height,channels,offset);
+          		break;
+						
+						case CONTRAST:
+          		runContrastKernel(setup,d_data,d_result,h_result,
+																width,height,channels,offset);
+          		break;
+						*/
+
+
+						case CONVOLUTION:
+							cudaMalloc((void**)&d_kernel,sizeKernel);
+							cudaMemcpy(d_kernel,h_kernel,sizeKernel,cudaMemcpyHostToDevice);
+							runConvolutionKernel(setup,d_data,d_result,h_result,d_kernel
+																width,height,channels,offset);
+          		cudaFree(d_kernel); 
+              break;
+						/*
+						case BLEND:
+							runBlendKernel(setup,d_data,d_result,h_result,
+                             width,height,channels,offset);
+
+							break;
+
+						case SATURATION:
+							runSaturationKernel(setup,d_data,d_result,h_result,
+          	                      width,height,channels,offset);
+							break;
+
+						case SEPIA:
+							runSepiaKernel(setup,d_data,d_result,h_result,
+                             width,height,channels,offset);
+							break;
+							*/
+
+					}
+					dTime2 = gpu::getTime(tim);
+					executionTime += dTime2 - dTime1;
+					std::cout << "Done with execution on GPU.\nTime Taken: " << dTime2 - dTime1	<< std::endl;
+
+					dTime1 = dTime2;					
+          cudaMemcpy(h_result,d_result,sizeResult,cudaMemcpyDeviceToHost);
+          cudaFree(d_data);
+          cudaFree(d_result);
+
+					dTime2 = gpu::getTime(tim);
+					configurationTime += dTime2 - dTime1;
+					std::cout << "Data transferred back to Host.\nTime taken: " << dTime2 - dTime1 << std::endl;
+					
+
+
     			CImg<unsigned char> outputImage(outputBuffer,imgInfo.width,imgInfo.height,1,
                                     imgInfo.spectrum,0);
     
-    			double dTime3 = gpu::getTime(tim);
-    			std::cout << "time taken for GPU operation: " << dTime3 - dTime2 << std::endl;
-   
+					dTime1 = dTime2;
+					
+					std::cout << "Writing to Disk"<<std::endl;	
 					std::string outputDirectory = directoryPath+"output/";
-          int outDir = mkdir(outputDirectory.c_str(),0777);
-          if(outDir == 0 || errno == EEXIST)
-          {	
-          	//std::cout << filename << std::endl;	
+					int outDir = mkdir(outputDirectory.c_str(),0777);
+          
+					if(outDir == 0 || errno == EEXIST)
+          {
           	outputImage.save_jpeg((directoryPath+"/output/"+outputFilename+extension).c_str());
+						dTime2 = gpu::getTime(tim);
+						fileIOTime += dTime2 - dTime1;
+						std::cout << "Time for Disk Write: " << dTime2 - dTime1 << std::endl;
           }
           else
           {
@@ -85,18 +196,13 @@ int main(int argc, char* argv[])
           	}
           }
 					
-					/*CImgDisplay darkDisplay(outputImage,"Output Image",0);
-                                                                   
-          while(!(mainDisplay.is_closed()))
-          {
-          	mainDisplay.wait();
-          }*/
-
-
- 
     			delete[] inputBuffer;
     			delete[] outputBuffer;
 			}
+			std::cout << "File I/O time: " << fileIOTime << std::endl;
+			std::cout << "Configuration time: " << configurationTime << std::endl;
+			std::cout << "Execution time: " << executionTime << std::endl;
+			std::cout << "GPU Utilization: " << (double)executionTime/(fileIOTime+configurationTime+executionTime) << std::endl;
 		}
   }
   else
